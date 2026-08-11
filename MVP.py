e1cb4ee3319f698aded9b83395a511e738f41c0a
ovicoon <ovicoon@gmail.com>
2026-08-11 23:10:52 +0900

    테스트용으로 MAX SAMPLES 를 5로 감소

--- MVP.py
+++ MVP.py
@@ -194,7 +194,7 @@ if __name__ == "__main__":
     ai = torch.compile(ai, mode="default")
     ai = ai.to(device=device)
 
-    MAX_SAMPLES = 100
+    MAX_SAMPLES = 5
 
     ai.tokenizer.padding_side = "left"
     if ai.tokenizer.pad_token is None:
