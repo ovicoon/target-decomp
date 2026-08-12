e8f387e767c69cbbaafe3167b33934e2af2aa1d9
ovicoon <ovicoon@gmail.com>
2026-08-13 02:33:44 +0900

    MAX SAMPLE 500 epoch 500

--- MVP.py
+++ MVP.py
@@ -234,7 +234,7 @@ if __name__ == "__main__":
     ai = torch.compile(ai, mode="default")
     ai = ai.to(device=device)
 
-    MAX_SAMPLES = 1000
+    MAX_SAMPLES = 500
 
     ai.tokenizer.padding_side = "left"
     if ai.tokenizer.pad_token is None:
@@ -312,7 +312,7 @@ if __name__ == "__main__":
     # 4. 학습 시작
     print("\n=== CSV 대화 데이터 기반 원샷 학습 시작 ===")
     ai.train()
-    epochs = 1000
+    epochs = 500
 
     # Warmup / Compile
     with torch.amp.autocast("cuda"):
